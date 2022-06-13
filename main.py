13bc2a7501842acf97175beb6a5546f5cea53e23
KacperGorski1703 <kacpergorski1703@gmail.com>
2022-06-13 20:31:53 +0200

    init commit

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 my_list = {
     "piekarnia": ["chleb", "bułki", "pączek"],
-    "warzywniak": ["marchew", "seler", "rukola"]
+    "warzywniak": ["marchew", "seler", "rukola", ]
+}
+for dict_key in my_list:
+    print(f"Idę do {dict_key.capitalize()} i kupuje tam  {my_list[dict_key]}")
+
+produkty = [len(my_list[dict_key])+len(my_list[dict_key])]
+print(f"W sumie kupuje {produkty} produktów")
